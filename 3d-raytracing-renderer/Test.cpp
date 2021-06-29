27c7d201c8b8756013a760e0f608f9659b79d249
Disembleergon <tom.pekrul1@gmail.com>
2021-06-29 17:24:49 +0200

    changed example in Test.cpp

--- 3d-raytracing-renderer/Test.cpp
+++ 3d-raytracing-renderer/Test.cpp
@@ -5,46 +5,31 @@ int main()
 
     World w{};
 
-    Sphere sp{};
+    Plane ground{};
 
-    auto sphereSolid1 = SolidPattern{highvalueColor(62, 107, 98)};
-    auto sphereSolid2 = SolidPattern{highvalueColor(178, 192, 161)};
-    auto sphereStripe =
-        StripePattern{makePattern_ptr<SolidPattern>(sphereSolid1), makePattern_ptr<SolidPattern>(sphereSolid2)};
+    // ---- pattern -----
 
-    sp.material.pattern =
-        makePattern_ptr<JitteredPattern>(JitteredPattern{makePattern_ptr<StripePattern>(sphereStripe), 0.6});
-    sp.material.pattern->setTransform(rotate_z(toRadians(50)) * scaling(0.4, 0.4, 0.4));
-    sp.material.specular = 0.9;
-    sp.material.diffuse = 0.7;
-    sp.material.ambient = 0.6;
-    sp.setTransform(translation(0, 1, 0));
-    w.objects.push_back(&sp);
+    SolidPattern grey1{highvalueColor(114, 115, 117)};
+    SolidPattern grey2{highvalueColor(73, 74, 77)};
+    StripePattern stripe1{makePattern_ptr<SolidPattern>(grey1), makePattern_ptr<SolidPattern>(grey2)};
+    stripe1.setTransform(scaling(0.2, 0.2, 0.2) * rotate_y(toRadians(45)));
 
-    Plane underground{};
+    SolidPattern pink1{highvalueColor(214, 39, 71)};
+    SolidPattern pink2{highvalueColor(255, 105, 132)};
+    StripePattern stripe2{makePattern_ptr<SolidPattern>(pink1), makePattern_ptr<SolidPattern>(pink2)};
+    stripe2.setTransform(scaling(0.2, 0.2, 0.2) * rotate_y(toRadians(-45)));
 
-    // pattern config
+    CheckersPattern checkers{makePattern_ptr<StripePattern>(stripe1), makePattern_ptr<StripePattern>(stripe2)};
+    ground.material.pattern = makePattern_ptr<CheckersPattern>(checkers);
 
-    auto solidOne = SolidPattern{highvalueColor(158, 201, 212)};
-    auto solidTwo = SolidPattern{highvalueColor(66, 91, 123)};
-    auto sOneStripe = StripePattern{makePattern_ptr<SolidPattern>(solidOne), makePattern_ptr<SolidPattern>(solidTwo)};
+    // ---- pattern end -----
 
-    auto sOne = JitteredPattern{makePattern_ptr<StripePattern>(sOneStripe), 0.6};
-    auto sTwo = StripePattern{makePattern_ptr(solidTwo), makePattern_ptr(solidOne)};
-
-    sTwo.setTransform(rotate_y(toRadians(90)) * scaling(0.7, 0.7, 0.7));
-    sOne.setTransform(scaling(0.7, 0.7, 0.7));
-
-    // pattern config end
-
-    underground.material.pattern = makePattern_ptr<BlendPattern>(
-        BlendPattern{makePattern_ptr<JitteredPattern>(sOne), makePattern_ptr<StripePattern>(sTwo)});
-    w.objects.push_back(&underground);
+    w.objects.push_back(&ground);
 
     w.light = PointLight{point(-10, 10, -5), Color{1, 1, 1}};
 
-    Camera cam{400, 200, M_PI / 3};
-    cam.setTransformation(view_transform(point(0, 1, -5), point(0, 1, 0), vector(0, 1, 0)));
+    Camera cam{1200, 600, M_PI / 3};
+    cam.setTransformation(view_transform(point(-1, 1.5, -1), point(2, 0, 5), vector(0, 1, 0)));
 
     w.render(cam).toPPM("C:\\Users\\tompe\\desktop\\scene.ppm");
 
